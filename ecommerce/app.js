e7680d226372a0489866736460c2e4f6ce303ffd
fullyalive <wogud1202@gmail.com>
2019-09-05 23:38:01 +0900

    setup | Express server

---
+++ ecommerce/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("hello from node");
+});
+
+const port = process.env.PORT || 7777;
+
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
